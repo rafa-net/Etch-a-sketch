c771baace83c76bbe1fcd4917132b47fdfae16ca
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-13 16:12:01 -0400

    First skeleton of the project laid out

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+const container = document.getElementById("squareContainer");
 
+function createNewGrid(size) {
+
+}
+
+// Handling of the creation of a new grid
+const resizeBtn = document.getElementById("resize-btn");
+resizeBtn.addEventListener("click", () => {
+  let size = prompt(`Please enter the desired board size:`);
+  createNewGrid(size);
+});
+
+
+
+// Creation of a new grid to start with
+createNewGrid(16);
