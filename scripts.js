a74390a3f7baf8ceb82f0e3fabeb0bed585a7ab3
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-13 17:02:46 -0400

    Add hover effect and have container squared

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const container = document.getElementById("squareContainer");
 
 // Everything happens through a dynamic loop here, without static values
 function createNewGrid(size) {
+    // Clears the container so there are no overlaps
+    container.innerHTML = "";
     // As long as the iteration variable is smaller than the result of 5 x 5, 16 x 16, increment it, so more squares are added
     for (let i = 0; i < size * size; i++) {
     let square = document.createElement("div");
@@ -10,16 +12,21 @@ function createNewGrid(size) {
     square.style.height = `${100 / size}%`;
     container.appendChild(square);
   }
+    // Add event listeners to the squares
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+      });
+    });
 }
 
-// Handling of the creation of a new grid
+// Handling of the creation of a 2nd new grid
 const resizeBtn = document.getElementById("resize-btn");
 resizeBtn.addEventListener("click", () => {
   let size = prompt(`Please enter the desired board size:`);
   createNewGrid(size);
 });
-
-
 
 // Creation of a new grid to start with
 createNewGrid(16);
