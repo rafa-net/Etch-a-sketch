fed577a8c8e7faa4f70f896dd265ffa2847471cd
Rafael Bueno <rbueno.100.b@gmail.com>
2024-05-13 16:33:28 -0400

    Add function to create grid Now it's possible for the user to choose the size of the grid

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
 const container = document.getElementById("squareContainer");
 
+// Everything happens through a dynamic loop here, without static values
 function createNewGrid(size) {
-
+    // As long as the iteration variable is smaller than the result of 5 x 5, 16 x 16, increment it, so more squares are added
+    for (let i = 0; i < size * size; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = `${100 / size}%`;
+    square.style.height = `${100 / size}%`;
+    container.appendChild(square);
+  }
 }
 
 // Handling of the creation of a new grid
